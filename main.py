9c536973e1cc0eaaf24dfcd4869e4d487d8f1184
Hyung Wook Choi <qpflapffhs76@gmail.com>
2023-12-30 16:15:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ URL="https://v2.velog.io/rss/ctdlog"
 RSS_FEED = feedparser.parse(URL)
 MAX_POST=5
 
-markdown_text = """
-### 📕 Latest Blog Posts </h3> 
-""" # list of blog posts will be appended here
+# markdown_text = """
+# ### 📕 Latest Blog Posts </h3> 
+# """ # list of blog posts will be appended here
 
 
-for idx, feed in enumerate(RSS_FEED['entries']):
-    if idx > MAX_POST:
-        break
-    else:
-        feed_date = feed['published_parsed']
-        markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
+# for idx, feed in enumerate(RSS_FEED['entries']):
+#     if idx > MAX_POST:
+#         break
+#     else:
+#         feed_date = feed['published_parsed']
+#         markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
 
 f = open("README.md",mode="w", encoding="utf-8")
-f.write(markdown_text)
+# f.write(markdown_text)
 f.close() 
